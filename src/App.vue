fa6a4aa04f883c45f70f1ba379fef853ddc0e203
zyhang <izyhang@gmail.com>
2018-10-11 16:55:04 +0800

    💥【modify】icon

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <img alt="Vue logo" src="./assets/yeah-215x215.png">
     <password-generator/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import PasswordGenerator from './components/PasswordGenerator.vue'
 
 export default {
   name: 'app',
   components: {
-    // HelloWorld
     PasswordGenerator
   }
 }
